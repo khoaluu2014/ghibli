9bf848a252aafd21ff39d2726a180e7110d05fea
khoaluu2014 <khoaluu2014@gmail.com>
2025-08-28 15:10:47 -0700

    fix script

--- color_picker/downloader.sh
+++ color_picker/downloader.sh
@@ -5,20 +5,19 @@ DEST="$HOME/Code/ghibli/color_picker/img/thewindrises"
 i=1
 
 while true; do
-  num=$(printf "%03d" $i)
-  url="https://www.ghibli.jp/gallery/kazetachinu${num}.jpg"
-  
-  http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
+	num=$(printf "%03d" $i)
+	url="https://www.ghibli.jp/gallery/kazetachinu${num}.jpg"
 
-  if["http_code" -ne 200]; then
-    echo "Stopping. Received ${http_code} from ${url}."
-    break
-  else
-    output="$DEST/thewindrises${num}.jpg"
-    echo "Downloading from ${url}"
-    curl -o "$output" "$url"
-  fi
+	http_code=$(curl -s -o /dev/null -w "%{http_code}" "$url")
 
-  i=$((i+1))
+	if [ "$http_code" -ne 200 ]; then
+		echo "Stopping. Received ${http_code} from ${url}."
+		break
+	else
+		output="$DEST/thewindrises${num}.jpg"
+		echo "Downloading from ${url}"
+		curl -o "$output" "$url"
+	fi
+
+	i=$((i + 1))
 done
-
